62b3c50bdb9459cee8880a6ebe057cef6f39d5d8
FuckthePopulation <ftpftpftp801@gmail.com>
2024-11-14 05:36:30 +0900

    лабораторная работа номер один

---
+++ triygolnik_labaratornaya_job_number1.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+
+
+
+
+//---------треугольник-----------
+bool triangle(double x, double y)
+{
+    if (x <= 3 && y <= 4 && y <= 4 - (4.0 / 3.0) * x)
+    {
+        if (x >= 0 && y >= 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(int argc, char* argv[])
+{
+    double x, y;
+
+    bool isHuman = false;
+    if (argc <= 1 || strcmp(argv[1], "false") != 0)
+    {
+        isHuman = true;
+    }
+
+    if (isHuman)
+    {
+        std::cout << "Enter coordinates X and Y, for your point" << "\n";
+    }
+
+    while (true)
+    {
+        cin >> x >> y;
+        if (x == 0 && y == 0)
+        {
+            break;
+        }
+        if (triangle(x, y))
+        {
+            if (isHuman)
+            {
+                cout << "Your point on triangle" << "\n";
+            }
+            else
+            {
+                cout << "Good" << "\n";
+            }
+        }
+        else
+        {
+            if (isHuman)
+            {
+                cout << "Nope, your point not on triangle" << "\n";
+            }
+            else
+            {
+                cout << "Nope" << "\n";
+            }
+        }
+    }
+}
